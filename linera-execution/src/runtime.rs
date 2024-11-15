e292143e06006278e2292f21f26cff5fb10eb31a
Thomas Chataigner <tom.chataigner@yahoo.fr>
2024-11-15 10:08:50 +0100

    feat: assert proof exists in runtime

--- linera-execution/src/runtime.rs
+++ linera-execution/src/runtime.rs
@@ -1041,20 +1041,20 @@ impl<UserInstance> BaseRuntime for SyncRuntimeInternal<UserInstance> {
         let prover = ProverClient::new();
         dbg!("---------------------------------------------------------------");
         println!("---------------------------------------------------------------");
-        let proof_bytes = self.read_data_blob(&proof_hash)?;
-        dbg!(proof_bytes.len());
-        println!("{}", proof_bytes.len());
-        // TODO standardize serde encoding
-        let verifying_key =
-            bcs::from_reader(vk.as_slice()).map_err(|error| ExecutionError::Bcs(error.into()))?;
-        let proof = bincode::deserialize_from(proof_bytes.as_slice())
-            .map_err(|error| ExecutionError::ServiceWriteAttempt)?;
-        dbg!(proof_bytes.len());
-        println!("{}", proof_bytes.len());
+        /*        let proof_bytes = self.read_data_blob(&proof_hash)?;
+                dbg!(proof_bytes.len());
+                println!("{}", proof_bytes.len());
+                // TODO standardize serde encoding
+                let verifying_key =
+                    bcs::from_reader(vk.as_slice()).map_err(|error| ExecutionError::Bcs(error.into()))?;
+                let proof = bincode::deserialize_from(proof_bytes.as_slice())
+                    .map_err(|error| ExecutionError::ServiceWriteAttempt)?;
+                dbg!(proof_bytes.len());
+                println!("{}", proof_bytes.len());
 
-        dbg!(prover.verify(&proof, &verifying_key).is_ok());
-
-        Ok(prover.verify(&proof, &verifying_key).is_ok())
+                dbg!(prover.verify(&proof, &verifying_key).is_ok());
+        */
+        Ok(self.assert_data_blob_exists(&proof_hash).is_ok())
     }
 }
 
