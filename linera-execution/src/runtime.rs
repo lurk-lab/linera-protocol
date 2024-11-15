ccdea56947b0cf42dfdf6a1da7bc49af2ebfe37d
Thomas Chataigner <tom.chataigner@yahoo.fr>
2024-11-15 09:39:34 +0100

    feat: more logs

--- linera-execution/src/runtime.rs
+++ linera-execution/src/runtime.rs
@@ -1039,7 +1039,7 @@ impl<UserInstance> BaseRuntime for SyncRuntimeInternal<UserInstance> {
         proof_hash: CryptoHash,
     ) -> Result<bool, ExecutionError> {
         let prover = ProverClient::new();
-
+        dbg!("---------------------------------------------------------------");
         let proof_bytes = self.read_data_blob(&proof_hash)?;
         dbg!(proof_bytes.len());
         println!("{}", proof_bytes.len());
