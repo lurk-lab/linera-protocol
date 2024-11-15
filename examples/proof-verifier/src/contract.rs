851e978b0af4feb5e245aafed2abb6168d9b7e5f
Thomas Chataigner <tom.chataigner@yahoo.fr>
2024-11-15 15:25:10 +0100

    feat: remove comments on FFI

--- examples/proof-verifier/src/contract.rs
+++ examples/proof-verifier/src/contract.rs
@@ -47,8 +47,8 @@ impl Contract for ProofVerificationContract {
 
     async fn execute_operation(&mut self, proof_hash: DataBlobHash) -> Self::Response {
         self.runtime.assert_data_blob_exists(proof_hash.clone());
-        /*let vk = self.state.verifying_key.get();
-        let res = self.runtime.verify_proof(vk, proof_hash);*/
+        let vk = self.state.verifying_key.get();
+        let res = self.runtime.verify_proof(vk, proof_hash);
         self.state.verified_proof.set(true);
     }
 
