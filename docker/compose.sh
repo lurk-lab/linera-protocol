83c1fe9c71c242dcca2b7fd9ffb0fc6f3b165888
Hanting Zhang <hantingz@usc.edu>
2025-03-10 12:12:34 -0400

    nix: docker fix

--- docker/compose.sh
+++ docker/compose.sh
@@ -64,9 +64,13 @@ cd "$SCRIPT_DIR"
 set -xe
 
 # Create configuration files.
+# * Private server states are stored in `server.json`.
+# * `committee.json` is the public description of the Linera committee.
 linera-server generate --validators "$CONF_DIR/validator.toml" --committee committee.json --testing-prng-seed 1
 
 # Create configuration files for 10 user chains.
+# * Private chain states are stored in one local wallet `wallet.json`.
+# * `genesis.json` will contain the initial balances of chains as well as the initial committee.
 linera --wallet wallet.json --storage rocksdb:linera.db create-genesis-config 10 --genesis genesis.json --initial-funding 10 --committee committee.json --testing-prng-seed 2
 
 if [ "${DOCKER_COMPOSE_WAIT:-false}" = "true" ]; then
